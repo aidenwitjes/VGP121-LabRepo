f25b46856553586431d41464b5b1146c8daead94
aidenwitjes <150625938+aidenwitjes@users.noreply.github.com>
2024-09-20 02:15:37 -0400

    Basic Audio + Player Controller
    Added the beginning stages of the audio set up and modified the player controller for underwater and land differences

--- VGP121Labs/Assets/Scripts/Player/PlayerController.cs
+++ VGP121Labs/Assets/Scripts/Player/PlayerController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer), typeof(Animator))]
 public class PlayerController : MonoBehaviour
@@ -73,35 +75,44 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private int maxLives = 99;
 
     //Movement Variables
-    [SerializeField, Range (1, 20)] private float speed = 5;
-    [SerializeField, Range(1, 20)] private float swimForce = 5;
+    [SerializeField, Range (1, 20)] private float swimSpeed = 5.0f;
+    [SerializeField, Range(1, 20)] private float maxSwimSpeed = 10.0f;
+    [SerializeField, Range(1, 20)] private float landSpeed = 7.0f;
+    [SerializeField, Range(1, 20)] private float jumpForce = 5.0f;
     [SerializeField, Range(0.01f, 1)] private float groundCheckRadius = 0.02f;
     [SerializeField] private LayerMask isGroundLayer;
 
+    [SerializeField] private float maxSinkVel = -5.0f; //Cap for sinking velocity
+    [SerializeField] private float maxSwimUpVel = 15.0f; //Cap for swimming up velocity
+
     private Transform groundCheck;
     private bool isGrounded = false;
+    private bool isUnderwater = false;
+    
+    //Can comment this out for final submission
+    private bool swimMessage = false; //Flag to if swimming is working properly
 
     Rigidbody2D rb;
     SpriteRenderer sr;
     Animator anim;
 
-    // Start is called before the first frame update
+    //Start is called before the first frame update
     void Start()
     {
        rb = GetComponent<Rigidbody2D>();
        sr = GetComponent<SpriteRenderer>();
        anim = GetComponent<Animator>();
 
-        if (speed <= 0)
+        if (swimSpeed <= 0)
         {
-            speed = 5;
+            swimSpeed = 5;
             Debug.Log("Speed was set incorrectly");
         }
         
-        if (swimForce<= 0)
+        if (jumpForce<= 0)
         {
-            swimForce = 5;
-            Debug.Log("SwimForce was set incorrectly");
+            jumpForce = 5;
+            Debug.Log("jumpForce was set incorrectly");
         }
 
         if (!groundCheck)
@@ -112,22 +123,82 @@ public class PlayerController : MonoBehaviour
             obj.name = "GroundCheck";
             groundCheck = obj.transform;
         }
+
+        //Check the scene to determine if the player is underwater
+        string sceneName = SceneManager.GetActiveScene().name;
+        isUnderwater = sceneName == "Level 1";
+       
+        //Can comment this out for final submission
+        if (SceneManager.GetActiveScene().name == "Level 1" && !swimMessage)
+        {
+            Debug.Log("The player is swimming.");
+            swimMessage = true;
+        }
+        else if (SceneManager.GetActiveScene().name != "Level 1")
+        {
+            swimMessage = false; //Reset flag
+        }
+
     }
 
-    // Update is called once per frame
+    //Update is called once per frame
     void Update()
     {
         float hInput = Input.GetAxis("Horizontal");
+        anim.SetFloat("hInput", Mathf.Abs(hInput));
 
-        rb.velocity = new Vector2(hInput * speed, rb.velocity.y);
-
-        if (Input.GetButtonDown("Jump"))
+        if (isUnderwater)
         {
-            rb.AddForce(Vector2.up * swimForce, ForceMode2D.Impulse);
+            //Underwater input
+            if (Input.GetButtonDown("Jump"))
+            {
+                rb.AddForce(Vector2.up * swimSpeed, ForceMode2D.Impulse);
+            }
+
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                rb.velocity = new Vector2(rb.velocity.x, -swimSpeed); // Swim down
+            }
+        }
+        else
+        {
+            //On-land input
+            rb.velocity = new Vector2(hInput * landSpeed, rb.velocity.y);
+            if (Input.GetButtonDown("Jump") && isGrounded)
+            {
+                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            }
         }
 
+        //Flip sprite based on input direction
         if (hInput != 0) sr.flipX = (hInput < 0);
+    }
 
-        anim.SetFloat("hInput", Mathf.Abs(hInput));
+    void FixedUpdate()
+    {
+        float hInput = Input.GetAxis("Horizontal");
+
+        if (isUnderwater)
+        {
+            //Swimming movement
+            rb.velocity = new Vector2(hInput * maxSwimSpeed, rb.velocity.y);
+
+            //Cap upward swimming velocity
+            if (rb.velocity.y > maxSwimUpVel)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, maxSwimUpVel);
+            }
+
+            //Cap sinking velocity
+            if (rb.velocity.y < maxSinkVel)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, maxSinkVel);
+            }
+        }
+        else
+        {
+            //On-land movement
+            rb.velocity = new Vector2(hInput * landSpeed, rb.velocity.y);
+        }
     }
 }
